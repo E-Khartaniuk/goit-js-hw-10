81c1baea1af1248d39ef280587700f8feaa1e75f
E-Khartaniuk <115401732+E-Khartaniuk@users.noreply.github.com>
2023-03-24 16:05:16 +0000

    Deploying to gh-pages from @ E-Khartaniuk/goit-js-hw-10@07831f6e97b813314b18a5b120911596aff18edb 🚀

---
+++ index.b276fdab.js
@@ -0,0 +1,2 @@
+a,fetch("https://restcountries.com/v3.1/all?fields=name,flags`");
+//# sourceMappingURL=index.b276fdab.js.map
---
+++ index.b4463b7a.js
@@ -0,0 +1,2 @@
+a,fetch("https://restcountries.com/v3.1/all?fields=name,flags`");
+//# sourceMappingURL=index.b4463b7a.js.map
