f2d0f0111c6d491c283638c5a2c66e38cf0ded14
Evhenii Khartaniuk <co6alt@gmail.com>
2023-03-27 16:23:29 +0300

    10

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const debounce = require('lodash.debounce');
-import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from '/notiflix/build/notiflix-notify-aio';
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
 import {
